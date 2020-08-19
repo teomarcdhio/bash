e820f0cbb0e9a924877bf4af9a3a3d9f73c5e0e1
teomarcdhio <mmarcolinishop@gmail.com>
2020-08-19 14:40:43 +0100

    Update nginxstatuscheck.sh
    Added Init info

--- nginxstatuscheck.sh
+++ nginxstatuscheck.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-
+### BEGIN INIT INFO
+# Provides:       nginxstatus
+# Required-Start:    \$local_fs \$syslog
+# Required-Stop:     \$local_fs \$syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts nginxstatus
+# Description:       starts nginxstatus using start-stop-daemon
+### END INIT INFO
 STATUS=$(systemctl is-active nginx)
 echo $STATUS
 while [ "$STATUS" != "active" ];
 do
         STATUS=$(systemctl is-active nginx)
         echo $STATUS
-        sleep 10
+        sleep 20
         sudo service nginx start 
 done
